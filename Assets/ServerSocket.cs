43604a6c133a98d68a9ca736ff6c861a3a749af9
gjwGithub <gujiawei_27@qq.com>
2015-07-13 09:04:20 +0800

    Initial

---
+++ Assets/ServerSocket.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class ServerSocket
+{
+    int count;
+    byte[] data;       
+    IPEndPoint ipep;
+    Socket newsock;
+    IPEndPoint sender;
+    EndPoint Remote;
+    bool isRunning = true;
+    Texture2D texture = null;
+	Thread thread = null;
+
+    public ServerSocket()
+    {  
+        data = new byte[102400];
+
+        //得到本机IP，设置TCP端口号         
+        ipep = new IPEndPoint(IPAddress.Any, 6000);
+        newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        //绑定网络地址
+        newsock.Bind(ipep);
+
+        //得到客户机IP
+        sender = new IPEndPoint(IPAddress.Any, 0);
+        Remote = (EndPoint)(sender);
+
+        ////客户机连接成功后，发送欢迎信息
+        //string welcome = "Welcome ! ";
+
+        ////字符串与字节数组相互转换
+        //data = Encoding.ASCII.GetBytes(welcome);
+
+        ////发送信息
+        //newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+
+        texture = new Texture2D(960, 720);
+
+        thread = new Thread(start);
+        thread.IsBackground = true;
+        thread.Start();
+		Debug.Log("Thread start");
+        
+    }
+
+    public void start()
+    {
+        while (isRunning)
+        {
+            //发送接受信息
+			//Debug.Log("Waiting for data");
+            count = newsock.ReceiveFrom(data, ref Remote);
+            byte[] des = new byte[count];
+			Debug.Log("count = "+count);
+
+			Array.Copy(des, data, count);
+            texture.LoadImage(des);
+
+            int ms = Environment.TickCount;
+            FileStream fs = new FileStream("/sdcard/test/" + ms + ".jpg", FileMode.Create);
+            BinaryWriter sw = new BinaryWriter(fs, Encoding.Default);
+            sw.Write(des);
+            sw.Close();
+            fs.Close();
+        }
+    }
+
+    public Texture2D getTexture()
+    {
+        return texture;
+    }
+
+	public void close()
+	{
+		isRunning = false;
+		thread.Abort();
+        newsock.Shutdown(SocketShutdown.Both);
+		newsock.Close();
+	}
+
+}
+
---
+++ Assets/WebCam.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class WebCam : MonoBehaviour {
+	
+	public string deviceName;
+	WebCamTexture tex;
+
+    ServerSocket server = null;
+
+    // Use this for initialization
+    IEnumerator Start ()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep; //屏幕常亮
+		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+	    if(Application.HasUserAuthorization(UserAuthorization.WebCam))
+	    {
+	    	WebCamDevice[] devices = WebCamTexture.devices;
+	    	deviceName = devices[0].name;
+	    	tex = new WebCamTexture(deviceName, 960, 720, 30);
+	    	GetComponent<Renderer>().material.mainTexture = tex;
+	    	tex.Play();
+	    }
+
+        server = new ServerSocket();
+    }
+
+	void Update ()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
+    void onGUI()
+    {
+        //Texture2D tex = server.getTexture();
+        //GetComponent<Renderer>().material.mainTexture = tex;
+    }
+
+    void onDestroy()
+    {
+        Debug.Log("Thread closed");
+        server.close();
+    }
+
+    void onApplicationQuit()
+    {
+        Debug.Log("Thread closed");
+        server.close();
+    }
+}
+
+
+
+
+
